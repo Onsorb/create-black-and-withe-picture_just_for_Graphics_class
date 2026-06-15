dfd95a8d8d8bef20da1152160b7b9692910c0d82
Ali <aabdalzadeh@gmail.com>
2026-06-15 14:23:43 +0330

    Add files via upload

---
+++ grafics/project.py
@@ -0,0 +1,62 @@
+import cv2
+import os
+
+# مسیر دقیق فایل (بر اساس خروجی dir که نشان می‌دهد فایل وجود دارد)
+image_path = "C:/Users/AsaGity/Downloads/pythonphoto.jpg"
+
+print(f"is loading: {image_path}")
+
+# بررسی وجود فایل با os.path
+if not os.path.exists(image_path):
+    print(f" Error ")
+    print("File located is analized :", image_path)
+    exit()
+
+# بارگذاری تصویر
+img = cv2.imread(image_path)
+
+if img is None:
+    print(" Unreadable!")
+    print("may not be supported.")
+    exit()
+
+print(f" successfully uploaded!")
+print(f"Dimensions: {img.shape[1]} x {img.shape[0]} ")
+
+gray_image = None
+is_gray = False
+
+cv2.imshow("Image processing - look at the console for commands", img)
+
+print("\n" + "="*50)
+print("Commands (activate the image in the window and press the keys):")
+print("   [g] → (Grayscale)")
+print("   [s] → save image gray.jpeg")
+print("   [ESC] یا [q] → Exit")
+print("="*50)
+
+while True:
+    key = cv2.waitKey(0) & 0xFF
+    
+    if key == ord('g'):
+        if not is_gray:
+            gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            cv2.imshow("Image path - look at the console for commands", gray_image)
+            is_gray = True
+            print(" The image has been converted to Grayscale! Now press [s] to save.")
+        else:
+            print(" The image is already Grayscale.")
+    
+    elif key == ord('s'):
+        if is_gray and gray_image is not None:
+            cv2.imwrite("gray.jpeg", gray_image)
+            full_path = os.path.abspath("gray.jpeg")
+            print(f"Saved: {full_path}")
+        else:
+            print(" First press the [g] key to turn the image gray!")
+    
+    elif key == 27 or key == ord('q'):  # ESC=27
+        print("|| Exit ||")
+        break
+
+cv2.destroyAllWindows()
